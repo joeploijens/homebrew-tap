06d10e08936a323eded3b049bab13c62e85fb925
Joep Loijens <joep.loijens@gmail.com>
2015-02-24 19:31:17 +0100

    postgresql 9.4.1

---
+++ postgresql.rb
@@ -0,0 +1,77 @@
+class Postgresql < Formula
+  homepage "http://www.postgresql.org/"
+  url "http://ftp.postgresql.org/pub/source/v9.4.1/postgresql-9.4.1.tar.bz2"
+  sha256 "29ddb77c820095b8f52e5455e9c6c6c20cf979b0834ed1986a8857b84888c3a6"
+
+  depends_on "openssl"
+  depends_on "readline"
+
+  # Don't strip fully expanded directory names already containing the string
+  # "postgres" or "pgsql".
+  patch :DATA
+
+  conflicts_with "postgres-xc",
+    :because => "postgresql and postgres-xc install the same binaries."
+
+  def install
+    ENV.libxml2 if MacOS.version >= :snow_leopard
+
+    args = %W[
+      --prefix=#{prefix}
+      --datadir=#{share}/#{name}
+      --docdir=#{doc}
+      --includedir=#{include}/#{name}
+      --sysconfdir=#{etc}
+      --enable-dtrace
+      --with-bonjour
+      --with-gssapi
+      --with-ldap
+      --with-libxml
+      --with-libxslt
+      --with-openssl
+      --with-uuid=e2fs
+      --with-pam
+      --with-perl
+      --with-python
+      --with-tcl
+    ]
+
+    system "./configure", *args
+    system "make", "install-world"
+  end
+
+  def post_install
+    # Make sure the run-time directories exist
+    %w[db/postgresql log run].each { |p| (var+p).mkpath }
+    (etc/"postgresql").mkpath
+  end
+
+  test do
+    system "#{bin}/pg_config"
+  end
+end
+
+__END__
+--- a/src/Makefile.global.in 2014-07-21 21:10:42.000000000 +0200
++++ b/src/Makefile.global.in 2014-10-27 13:37:59.000000000 +0100
+@@ -101,20 +101,12 @@
+ libdir := @libdir@
+ 
+ pkglibdir = $(libdir)
+-ifeq "$(findstring pgsql, $(pkglibdir))" ""
+-ifeq "$(findstring postgres, $(pkglibdir))" ""
+ override pkglibdir := $(pkglibdir)/postgresql
+-endif
+-endif
+ 
+ includedir := @includedir@
+ 
+ pkgincludedir = $(includedir)
+-ifeq "$(findstring pgsql, $(pkgincludedir))" ""
+-ifeq "$(findstring postgres, $(pkgincludedir))" ""
+ override pkgincludedir := $(pkgincludedir)/postgresql
+-endif
+-endif
+ 
+ mandir := @mandir@
+ 
