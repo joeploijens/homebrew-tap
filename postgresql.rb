3c85305c22511a53278b0d696002f6acec848667
Joep Loijens <joep.loijens@gmail.com>
2015-05-22 16:18:44 +0200

    postgresql 9.4.2

--- postgresql.rb
+++ postgresql.rb
@@ -1,7 +1,7 @@
 class Postgresql < Formula
-  homepage "http://www.postgresql.org/"
-  url "http://ftp.postgresql.org/pub/source/v9.4.1/postgresql-9.4.1.tar.bz2"
-  sha256 "29ddb77c820095b8f52e5455e9c6c6c20cf979b0834ed1986a8857b84888c3a6"
+  homepage "https://www.postgresql.org/"
+  url "https://ftp.postgresql.org/pub/source/v9.4.2/postgresql-9.4.2.tar.bz2"
+  sha256 "81fda191c165ba1d25d75cd0166ece5abdcb4a7f5eca01b349371e279ebb4d11"
 
   depends_on "openssl"
   depends_on "readline"
