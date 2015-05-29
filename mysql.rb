db24aa37fe18764163136fc95f7c20926143d76a
Joep Loijens <joep.loijens@gmail.com>
2015-05-29 17:26:17 +0200

    mysql 5.6.25

--- mysql.rb
+++ mysql.rb
@@ -1,7 +1,7 @@
 class Mysql < Formula
   homepage "https://dev.mysql.com/doc/refman/5.6/en/"
-  url "https://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.24.tar.gz"
-  sha256 "37e27305b67d76883c5902dce59c89d596beee9dca7dbadd4a2e117f8101dfeb"
+  url "https://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.25.tar.gz"
+  sha256 "15079c0b83d33a092649cbdf402c9225bcd3f33e87388407be5cdbf1432c7fbd"
 
   depends_on :macos => :mountain_lion
   depends_on "cmake" => :build
