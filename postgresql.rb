79408efb043566789fa2b4a8d35180df077f0184
Joep Loijens <joep.loijens@gmail.com>
2015-06-13 14:45:02 +0200

    postgresql 9.4.4

--- postgresql.rb
+++ postgresql.rb
@@ -1,8 +1,8 @@
 class Postgresql < Formula
   desc "Object-relational database system"
   homepage "https://www.postgresql.org/"
-  url "https://ftp.postgresql.org/pub/source/v9.4.3/postgresql-9.4.3.tar.bz2"
-  sha256 "734acfd2ac352dd0cd353d226f3199c2e1a2730200998aea5b38cf685136d755"
+  url "https://ftp.postgresql.org/pub/source/v9.4.4/postgresql-9.4.4.tar.bz2"
+  sha256 "538ed99688d6fdbec6fd166d1779cf4588bf2f16c52304e5ef29f904c43b0013"
 
   depends_on "openssl"
   depends_on "readline"
