03fe8fd1f5bd3b821eb60035f56bf7d08d62b13a
Joep Loijens <joep.loijens@gmail.com>
2016-04-22 12:53:17 +0200

    sqlcl 4.2.0.16.112.0616

--- Formula/sqlcl.rb
+++ Formula/sqlcl.rb
@@ -20,9 +20,9 @@ end
 class Sqlcl < Formula
   desc "Free, Java-based command-line interface for Oracle databases"
   homepage "https://www.oracle.com/technetwork/developer-tools/sql-developer/overview/index.html"
-  url "file://#{HOMEBREW_CACHE}/sqlcl-4.2.0.16.049.0843-no-jre.zip",
+  url "file://#{HOMEBREW_CACHE}/sqlcl-4.2.0.16.112.0616-no-jre.zip",
     :using => CacheDownloadStrategy
-  sha256 "4215ba3ca3ba57e213e837d3bb1be2e46c5596e5366aef553d89869c2506fd65"
+  sha256 "12231b6478b1c988f7a138968effc52151d49de236db2eb02956a6cee45d0c18"
 
   bottle :unneeded
 
