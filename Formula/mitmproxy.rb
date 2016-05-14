27f21ed17c49542f31a168604d5c1d76a2a8009f
Joep Loijens <joep.loijens@gmail.com>
2016-05-14 10:24:35 +0200

    mitmproxy: fix download url

--- Formula/mitmproxy.rb
+++ Formula/mitmproxy.rb
@@ -1,7 +1,7 @@
 class Mitmproxy < Formula
   desc "Intercept, modify, replay, save HTTP/S traffic"
   homepage "https://mitmproxy.org"
-  url "https://github.com/mitmproxy/mitmproxy/releases/download/v0.17/mitmproxy-0.17.1-osx.tar.gz"
+  url "https://github.com/mitmproxy/mitmproxy/releases/download/v0.17.1/mitmproxy-0.17.1-osx.tar.gz"
   sha256 "dd5b2779f078c3208c02c673ee6343bf7aedf1c1759df20f8103241147d833ea"
 
   bottle :unneeded
