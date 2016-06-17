7a04a7756ecabb3b8b3bf81d83eec54bedbee446
Joep Loijens <joep.loijens@gmail.com>
2016-06-17 13:34:44 +0200

    instant-client: Re-release with a connectivity fix

--- Formula/instant-client.rb
+++ Formula/instant-client.rb
@@ -22,12 +22,12 @@ class InstantClient < Formula
   homepage "https://www.oracle.com/technetwork/topics/intel-macsoft-096467.html"
   url "file://#{HOMEBREW_CACHE}/instantclient-basic-macos.x64-12.1.0.2.0.zip",
     :using => CacheDownloadStrategy
-  sha256 "8c72abe1ee29c6f85c82851cfe5cdfc31bff09e162438aa755fc76f7a78bd1b6"
+  sha256 "ecbf84ff011fcd8981c2cd9355f958ee42b2e452ebaad2d42df7b226903679cf"
 
   resource "sqlplus" do
     url "file://#{HOMEBREW_CACHE}/instantclient-sqlplus-macos.x64-12.1.0.2.0.zip",
       :using => CacheDownloadStrategy
-    sha256 "8eb5bc4da372c6d58de3b3af30a5736a392278272ea04d4400e0b3926a9bf6b8"
+    sha256 "d1a83949aa742a4f7e5dfb39f5d2b15b5687c87edf7d998fe6caef2ad4d9ef6d"
   end
 
   bottle :unneeded
