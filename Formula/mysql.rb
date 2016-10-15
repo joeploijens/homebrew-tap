2ada8edc1593c5de7d60e11007ac5faec13221dc
Joep Loijens <joep.loijens@gmail.com>
2016-10-15 15:54:42 +0200

    mysql 5.7.16

--- Formula/mysql.rb
+++ Formula/mysql.rb
@@ -1,8 +1,8 @@
 class Mysql < Formula
   desc "Relational database management system"
   homepage "https://dev.mysql.com/doc/refman/5.7/en/"
-  url "https://cdn.mysql.com/Downloads/MySQL-5.7/mysql-boost-5.7.15.tar.gz"
-  sha256 "7342a3a3e40878378dfaee252d42a3a5b06c58237f49c2544424d27316738945"
+  url "https://cdn.mysql.com/Downloads/MySQL-5.7/mysql-boost-5.7.16.tar.gz"
+  sha256 "43fc282f807353ff77ead21efb5f85f7f214c2a5362762a8cc370ae1c075095a"
 
   depends_on "cmake" => :build
   depends_on macos: :mavericks
