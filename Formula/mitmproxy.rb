9960e26567a4e624ec8f830e7368c009beb33571
Joep Loijens <joep.loijens@gmail.com>
2016-12-29 15:00:40 +0100

    mitmproxy 1.0.2

--- Formula/mitmproxy.rb
+++ Formula/mitmproxy.rb
@@ -1,8 +1,8 @@
 class Mitmproxy < Formula
   desc "Intercept, modify, replay, save HTTP/S traffic"
   homepage "https://mitmproxy.org"
-  url "https://github.com/mitmproxy/mitmproxy/releases/download/v1.0.1/mitmproxy-1.0.1-osx.tar.gz"
-  sha256 "d760fd2a54dbcc042b12e390c7da9a1f4f1b748cdd80ed578c5cf8ba884d1ede"
+  url "https://github.com/mitmproxy/mitmproxy/releases/download/v1.0.2/mitmproxy-1.0.2-osx.tar.gz"
+  sha256 "5752b482f7dc574b3f1f96c4bac1508c05b80a751f7e65bdf4900a1b1d891977"
 
   bottle :unneeded
 
