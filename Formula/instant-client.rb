1d71be536ed7d55cec6038313ad629392fcb180f
Joep Loijens <joep.loijens@gmail.com>
2019-09-22 13:10:09 +0200

    instant-client: 19.3.0.0.0

--- Formula/instant-client.rb
+++ Formula/instant-client.rb
@@ -1,53 +1,34 @@
-class CacheDownloadStrategy < CurlDownloadStrategy
-  def fetch
-    archive = @url.sub(%r[^file://], "")
-    unless File.exists?(archive)
-      odie <<~EOS
-        Formula expects to locate the following archive:
-          #{Pathname.new(archive).basename}
-
-        in the HOMEBREW_CACHE directory:
-          #{HOMEBREW_CACHE}
-
-        Copy the archive to the cache or create a symlink in the cache to the archive:
-          ln -sf /path/to/archive $(brew --cache)/
-      EOS
-    end
-    super
-  end
-end
-
 class InstantClient < Formula
   desc "Free, light-weight client software for connecting to Oracle databases"
-  homepage "https://www.oracle.com/technetwork/topics/intel-macsoft-096467.html"
-  url "file://#{HOMEBREW_CACHE}/instantclient-basic-macos.x64-12.2.0.1.0-2.zip",
-    using: CacheDownloadStrategy
-  sha256 "3ed3102e5a24f0da638694191edb34933309fb472eb1df21ad5c86eedac3ebb9"
-  version "12.2.0.1.0"
+  homepage "https://www.oracle.com/database/technologies/instant-client.html"
+  url "https://download.oracle.com/otn_software/mac/instantclient/193000/instantclient-basic-macos.x64-19.3.0.0.0dbru.zip"
+  sha256 "f4335c1d53e8188a3a8cdfb97494ff87c4d0f481309284cf086dc64080a60abd"
+  version "19.3.0.0.0"
 
   resource "sqlplus" do
-    url "file://#{HOMEBREW_CACHE}/instantclient-sqlplus-macos.x64-12.2.0.1.0-2.zip",
-      using: CacheDownloadStrategy
-    sha256 "d147cbb5b2a954fdcb4b642df4f0bd1153fd56e0f56e7fa301601b4f7e2abe0e"
+    url "https://download.oracle.com/otn_software/mac/instantclient/193000/instantclient-sqlplus-macos.x64-19.3.0.0.0dbru.zip"
+    sha256 "f7565c3cbf898b0a7953fbb0017c5edd9d11d1863781588b7caf3a69937a2e9e"
   end
 
   bottle :unneeded
 
-  depends_on macos: :el_capitan
+  depends_on macos: :high_sierra
 
   def install
     buildpath.install resource("sqlplus")
 
     # Fix permissions
     chmod "u+w,go-w", Dir["./*"]
-    chmod "u+w,a-x", "glogin.sql"
+    chmod "u+w,a-x", Dir["*_LICENSE", "glogin.sql"]
 
     # Fix install names
-    system "install_name_tool", "-id", "@rpath/liboramysql12.dylib", "liboramysql12.dylib"
+    system "install_name_tool", "-id", "@rpath/liboramysql19.dylib", "liboramysql19.dylib"
 
-    prefix.install Dir["*_README"]
+    prefix.install Dir["*_README", "*_LICENSE]
     bin.install %W[adrci genezi uidrvci sqlplus]
     lib.install Dir["*.dylib*"]
+    (prefix/"network/admin").mkpath
+    (prefix/"network/admin").install "README"
     (prefix/"sqlplus/admin").mkpath
     (prefix/"sqlplus/admin").install "glogin.sql"
   end
