0bcd2aa4186230368fa854715404babcfab55a72
Joep Loijens <joep.loijens@gmail.com>
2019-12-17 15:23:01 +0100

    mitmproxy 5.0.0

--- Formula/mitmproxy.rb
+++ Formula/mitmproxy.rb
@@ -1,9 +1,9 @@
 class Mitmproxy < Formula
   desc "Intercept, modify, replay, save HTTP/S traffic"
   homepage "https://mitmproxy.org"
-  version "4.0.4"
+  version "5.0.0"
   url "https://snapshots.mitmproxy.org/#{version}/mitmproxy-#{version}-osx.tar.gz"
-  sha256 "5e235c4cea420df67a1133e0c7eb7d56fac59575e93a1969823c343697333492"
+  sha256 "da3c64df16705b27e852329cb3b4fc216e4239a754b0585dc8bf824e4467b449"
 
   bottle :unneeded
 
