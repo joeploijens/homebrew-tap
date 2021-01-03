556d94aea4458346517ef1a99ab8a566eef7b8d8
Joep Loijens <joep.loijens@gmail.com>
2021-01-03 14:19:58 +0100

    sqlcl 20.3.0.274.1916

--- Formula/sqlcl.rb
+++ Formula/sqlcl.rb
@@ -20,26 +20,27 @@ end
 class Sqlcl < Formula
   desc "Free, Java-based command-line interface for Oracle databases"
   homepage "https://www.oracle.com/database/technologies/appdev/sqlcl.html"
-  url "file://#{HOMEBREW_CACHE}/sqlcl-20.2.0.174.1557.zip",
+  url "file://#{HOMEBREW_CACHE}/sqlcl-20.3.0.274.1916.zip",
     using: CacheDownloadStrategy
-  sha256 "5290a737c8e01b4296a704dcef1fe94abf960defc02bd24c272773bba9386b30"
+  sha256 "5ff66a0704d8cd117d77045c4a877d12447ff14a7ae1e4dfcbfd5a56d0b72eb0"
 
   bottle :unneeded
 
-  depends_on java: "11"
+  depends_on "openjdk@11"
 
   def install
     # Remove Windows files
     rm_f "bin/sql.exe"
 
     prefix.install "README.md"
+    rm_f "20.3.0.274.1916"
     rm_f "bin/README.md"
     rm_f "bin/dependencies.txt"
     rm_f "bin/version.txt"
 
     bin.install "bin/sql" => "sqlcl"
     libexec.install "lib"
-    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("11"))
+    bin.env_script_all_files(libexec/"bin", JAVA_HOME: Formula["openjdk@11"].opt_prefix)
   end
 
   test do
