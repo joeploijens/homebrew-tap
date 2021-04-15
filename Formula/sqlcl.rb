9a17923e4be5f0e977f0529f3c64ae3279036397
Jonathan Hult <jonathan@jonathanhult.com>
2021-04-15 10:52:05 -0400

    Bump sqlcl to 21.1.0.104.1544

--- Formula/sqlcl.rb
+++ Formula/sqlcl.rb
@@ -1,5 +1,5 @@
 class CacheDownloadStrategy < CurlDownloadStrategy
-  def fetch
+  def fetch(timeout: nil, **options)
     archive = @url.sub(%r[^file://], "")
     unless File.exists?(archive)
       odie <<~EOS
@@ -20,28 +20,28 @@ end
 class Sqlcl < Formula
   desc "Free, Java-based command-line interface for Oracle databases"
   homepage "https://www.oracle.com/database/technologies/appdev/sqlcl.html"
-  url "file://#{HOMEBREW_CACHE}/sqlcl-20.4.1.351.1718.zip",
+  url "file://#{HOMEBREW_CACHE}/sqlcl-21.1.0.104.1544.zip",
     using: CacheDownloadStrategy
-  sha256 "7d69d50408e010c223e6f6a3efbf8b9d20832032219d53afe36a84707ba5073b"
+  sha256 "39469ff39c91c116b48895d56cbfc837633e0fbc1a4605d6e513486f139260ef"
 
   bottle :unneeded
 
   depends_on arch: :x86_64
-  depends_on "openjdk@11"
+  depends_on "java"
 
   def install
     # Remove Windows files
     rm_f "bin/sql.exe"
 
     prefix.install "README.md"
-    rm_f "20.4.1.351.1718"
+    rm_f "21.1.0.104.1544"
     rm_f "bin/README.md"
     rm_f "bin/dependencies.txt"
     rm_f "bin/version.txt"
 
     bin.install "bin/sql" => "sqlcl"
     libexec.install "lib"
-    bin.env_script_all_files(libexec/"bin", JAVA_HOME: Formula["openjdk@11"].opt_prefix)
+    bin.env_script_all_files(libexec/"bin", JAVA_HOME: Formula["java"].opt_prefix)
   end
 
   test do
