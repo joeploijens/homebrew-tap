c80e26c39652e3f04890424ad4c0794be3f71204
Joep Loijens <joep.loijens@gmail.com>
2021-05-22 20:45:43 +0200

    unrar 6.0.6

--- Formula/unrar.rb
+++ Formula/unrar.rb
@@ -1,14 +1,15 @@
 class Unrar < Formula
   desc "Extract, view, and test RAR archives"
   homepage "https://www.rarlab.com/"
-  url "https://www.rarlab.com/rar/unrarsrc-6.0.4.tar.gz"
-  sha256 "130197e495d6e2c2ee790a5beee123edeed642508be13f0159672e5397aca6c1"
+  url "https://www.rarlab.com/rar/unrarsrc-6.0.6.tar.gz"
+  sha256 "011ef7290d3394a62bb5bfced914cd510a7eea7255cf69417f9c952bb6056588"
+  license :cannot_represent
 
   def install
     # Upstream doesn't particularly care about their unix targets,
     # so we do the dirty work of renaming their shared objects to
     # dylibs for them.
-    inreplace "makefile", "libunrar.so", "libunrar.dylib"
+    inreplace "makefile", "libunrar.so", "libunrar.dylib" if OS.mac?
 
     system "make"
     bin.install "unrar"
@@ -18,6 +19,16 @@ class Unrar < Formula
     system "make", "clean"
     system "make", "lib"
     lib.install shared_library("libunrar")
+
+    prefix.install "license.txt"
+  end
+
+  def caveats
+    <<~EOS
+      We agreed to the UnRAR license for you:
+        #{opt_prefix}/license.txt
+      If this is unacceptable you should uninstall the formula.
+    EOS
   end
 
   test do
