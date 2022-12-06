652fcd6f5c6b3a1ddcbe945b5c50b0a3438eff28
Joep Loijens <joep.loijens@gmail.com>
2022-12-06 20:34:08 +0100

    graphicsmagick 1.3.38

---
+++ Formula/graphicsmagick.rb
@@ -0,0 +1,51 @@
+class Graphicsmagick < Formula
+  desc "Image processing tools collection"
+  homepage "http://www.graphicsmagick.org/"
+  url "https://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/1.3.38/GraphicsMagick-1.3.38.tar.xz"
+  sha256 "d60cd9db59351d2b9cb19beb443170acaa28f073d13d258f67b3627635e32675"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "freetype"
+  depends_on "jpeg-turbo"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "libtool"
+  depends_on "little-cms2"
+  depends_on "webp"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  skip_clean :la
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-openmp
+      --disable-static
+      --enable-shared
+      --with-modules
+      --with-quantum-depth=16
+      --without-lzma
+      --without-x
+      --without-gslib
+      --with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts
+      --without-jasper
+      --without-jxl
+      --without-wmf
+    ]
+
+    # versioned stuff in main tree is pointless for us
+    inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_VERSION}", "${PACKAGE_NAME}"
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    fixture = test_fixtures("test.png")
+    assert_match "PNG 8x8+0+0", shell_output("#{bin}/gm identify #{fixture}")
+  end
+end
